f6f710dca4ac1d2bd6c17adf3df64a3f3d96e658
Kevin Dallian <kevindallian@gmail.com>
2023-07-18 23:43:01 +0700

    added swipe gesture handler

--- NanoChallenge3/Game/GameViewController.swift
+++ NanoChallenge3/Game/GameViewController.swift
@@ -9,15 +9,36 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSKView()
+        setupSwipedGestureRecognizer()
+    }
+    
+    private func setupSKView(){
         let skView = SKView(frame: self.view.bounds)
         self.view.addSubview(skView)
         let gameScene = GameScene(size: skView.bounds.size)
         skView.presentScene(gameScene)
-
-        // Do any additional setup after loading the view.
+    }
+    
+    func setupSwipedGestureRecognizer() {
+        let rightSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipedRight))
+        rightSwipeGesture.direction = .right
+        view.addGestureRecognizer(rightSwipeGesture)
+        
+        let leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipedLeft))
+        leftSwipeGesture.direction = .left
+        view.addGestureRecognizer(leftSwipeGesture)
+    }
+    
+    @objc func swipedRight() {
+       print("Swiped right")
+    }
+    
+    @objc func swipedLeft() {
+       print("Swiped left")
     }
 
 }
